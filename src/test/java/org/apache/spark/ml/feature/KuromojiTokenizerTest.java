f10a81708670e10f6aa4ad440ff43ca203eef574
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2016-06-21 16:16:49 +0900

    Fix Java test

--- src/test/java/org/apache/spark/ml/feature/KuromojiTokenizerTest.java
+++ src/test/java/org/apache/spark/ml/feature/KuromojiTokenizerTest.java
@@ -56,15 +56,14 @@ public class KuromojiTokenizerTest {
 
   @Test
   public void testRun() {
-    List<String> data = Arrays.asList(
-        "天皇は、日本国の象徴であり日本国民統合の象徴であつて、この地位は、主権の存する日本国民の総意に基く。",
-        "皇位は、世襲のものであつて、国会の議決した皇室典範 の定めるところにより、これを継承する。",
-        "天皇の国事に関するすべての行為には、内閣の助言と承認を必要とし、内閣が、その責任を負ふ。",
-        "天皇は、この憲法の定める国事に関する行為のみを行ひ、国政に関する権能を有しない。",
-        "天皇は、法律の定めるところにより、その国事に関する行為を委任することができる。");
-    JavaRDD<Row> rdd = jsc.parallelize(data).map((String text) -> {
-      return RowFactory.create(text);
-    });
+    List<Row> data = Arrays.asList(
+        RowFactory.create("天皇は、日本国の象徴であり日本国民統合の象徴であつて、この地位は、主権の存する日本国民の総意に基く。"),
+        RowFactory.create("皇位は、世襲のものであつて、国会の議決した皇室典範 の定めるところにより、これを継承する。"),
+        RowFactory.create("天皇の国事に関するすべての行為には、内閣の助言と承認を必要とし、内閣が、その責任を負ふ。"),
+        RowFactory.create("天皇は、この憲法の定める国事に関する行為のみを行ひ、国政に関する権能を有しない。"),
+        RowFactory.create("天皇は、法律の定めるところにより、その国事に関する行為を委任することができる。")
+        );
+    JavaRDD<Row> rdd = jsc.parallelize(data);
     StructType schema = DataTypes.createStructType(new StructField[]{
         DataTypes.createStructField("text", DataTypes.StringType, false)});
     DataFrame df = jsql.createDataFrame(rdd, schema);
